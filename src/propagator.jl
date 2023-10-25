5133703bd06b310f4aef7b86f90bf6dc6d783eda
Sofia Catalan <sofia.catalan@utexas.edu>
2023-10-24 19:01:11 -0500

    updates to propagator

--- src/propagator.jl
+++ src/propagator.jl
@@ -70,21 +70,33 @@ end
 #     return kep
 # end
 
+function orbitPeriod(kep, mu)
+    T = 2.0 * pi * sqrt(kep[1]^3 / mu)
+    return T
+end
 
 mu = 398600.4415
 r = 6378.0
-x0 = kep2cart([r+400.0, 0.0, 51.6*pi/180, 0.0, 0.0, 0.0], mu)
-println(x0)
-t = (0.0, 90*60.0)
-prob = propagate_2Body(x0, t, mu)
+kep0_p1 = [r+400.0, 0.0, 51.6*pi/180, 0.0, 0.0, 0.0]
+kep0_p2 = [42164, 0.0, 0.0, 0.0, 0.0, 0.0]
+t = (0.0, orbitPeriod(kep0_p2, mu))
+prop1 = propagate_2Body(kep2cart(kep0_p1, mu), t, mu)
+prop2 = propagate_2Body(kep2cart(kep0_p2, mu), t, mu)
 
-xVals = []
-yVals = []
-zVals = []
+xVals_p1 = []
+yVals_p1 = []
+zVals_p1 = []
+xVals_p2 = []
+yVals_p2 = []
+zVals_p2 = []
 for i in 1:length(prob.u)
-    push!(xVals, prob.u[i][1])
-    push!(yVals, prob.u[i][2])
-    push!(zVals, prob.u[i][3])
+    push!(xVals_p1, prop1.u[i][1])
+    push!(yVals_p1, prop1.u[i][2])
+    push!(zVals_p1, prop1.u[i][3])
+    push!(xVals_p2, prop2.u[i][1])
+    push!(yVals_p2, prop2.u[i][2])
+    push!(zVals_p2, prop2.u[i][3])
 end
 
-scatter(xVals, yVals, zVals)
+scatter(xVals_p1, yVals_p1, zVals_p1, label="p1")
+scatter!(xVals_p2, yVals_p2, zVals_p2, label="p2")
