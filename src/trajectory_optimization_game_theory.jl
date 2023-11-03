71e3fa352d90bab39e59c044bff67d3cee614770
Sofia Catalan <sofia.catalan@utexas.edu>
2023-11-03 13:44:13 -0500

    adding updates for 3d tag example. test runs but no working plots yet, only constant accel dynamics in script for now

--- src/trajectory_optimization_game_theory.jl
+++ src/trajectory_optimization_game_theory.jl
@@ -1,6 +1,18 @@
 module trajectory_optimization_game_theory
 
+using Symbolics: Symbolics, @variables
+using ParametricMCPs: ParametricMCPs, ParametricMCP
+using BlockArrays: BlockArray, Block, mortar, blocks
+using LinearAlgebra: norm_sqr
+using LazySets: LazySets
+
 # Write your package code here.
 include("propagator.jl") 
+
+include("parametric_game.jl")
+export ParametricGame, total_dim, solve
+
+include("environment.jl")
+export get_constraints, CubeEnvironment
 
 end
