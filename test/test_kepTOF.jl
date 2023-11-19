2526d04ae3621a9acedd59489c11afde6be496fa
mis0butter <junetter@gmail.com>
2023-11-18 23:25:04 -0800

    test_kepTOF need to deal with hyperbolic orbit

--- test/test_kepTOF.jl
+++ test/test_kepTOF.jl
@@ -3,8 +3,8 @@ using LinearAlgebra
 
 ## ============================================ ## 
 
-r1      = [20.0e6, 20.0e6, 0]   # [m] 
-r2      = [-20.0e6, 10.0e6, 0]  # [m] 
+r0      = [20.0e6, 20.0e6, 0]   # [m] 
+rf      = [-20.0e6, 10.0e6, 0]  # [m] 
 tof     = 1.0 * 86400 
 mu      = 398600.4418e9         # [m^3/s^2] 
 dm      = "pro" 
@@ -13,13 +13,14 @@ Dtsec   = tof
 ## ============================================ ##
 # solve and propagate lambert orbit 
 
-v1, v2 = lambertbattin( r1, r2, mu, dm, tof ) 
+v0, vf = lambertbattin( r0, rf, mu, dm, tof ) 
 
-rv0 = [ r1; v1 ] 
+rv0 = [ r0; v0 ] 
 t_lambert, rv_lambert = propagate_2Body( rv0, tof, mu, 1.0 ) 
 rv_lambert = mapreduce( permutedims, vcat, rv_lambert ) 
 
 ## ============================================ ##
+# plot 
 
 fig = plot_orbit( rv_lambert ) 
 
@@ -27,10 +28,10 @@ fig = plot_orbit( rv_lambert )
 # break up delta v into smaller segments 
 
 # initial position has all z velocity 
-vz  = [ 0; 0; norm(v1) ]
+vz  = [ 0; 0; norm(v0) ]
 
 # compute delta v vec for lambert solution 
-dv  = v1 - vz   
+dv  = v0 - vz   
 
 # break up delta v into smaller segments 
 N = 10 
@@ -43,11 +44,64 @@ dv_vec = mapreduce( permutedims, vcat, dv_vec )
 ## ============================================ ##
 # use Kepler TOF equations to propagate each segment 
 
-i = 1 
-
-rv = [ r1; v1 ]  
 dt_N = tof / N 
+rv0  = [ r0; v0 ]  
+rvk  = rv0 
 
+rv_hist = [ rvk + [ zeros(3) ; dv_vec[i,:] ] ] 
+for i = 1 : N 
+
+    println( "i = ", i ) 
+
+    # apply delta v 
+    rvk[4:6] = rvk[4:6] + dv_vec[i,:] 
+
+    # orbital elements 
+    oek = cart2kep( rvk, mu ) 
+    a   = oek[1]                # semimajor axis 
+    e   = oek[2]                # eccentricity       
+    n   = sqrt( mu / a^3 )      # mean motion 
+    M   = n * dt_N              # mean anomaly 
+    E   = kepler_E( M, e )    # eccentric anomaly  
+    
+    println( "e = ", e ) 
+    println( "E = ", E ) 
+
+    # compute nu from eccentric anomaly 
+    # nu = 2 * atan( sqrt( (1+e)/(1-e) ) * tan(E/2) ) 
+    nu = acos( ( cos(E) - e ) / ( 1 - e*cos(E) ) ) 
+    oek[6] = nu 
+
+    rvk = kep2cart( oek, mu ) 
+    push!( rv_hist, rvk ) 
+
+end 
+rv_hist = mapreduce( permutedims, vcat, rv_hist ) 
+
+
+
+## ============================================ ##
+
+# Function solves Kepler's equation M = E-e*sin(E)
+# Input - Mean anomaly M [rad] , Eccentricity e and Epsilon 
+# Output  eccentric anomaly E [rad]. 
+function kepler_E( 
+    M, 
+    e, 
+    eps = 1e-10 
+    )
+
+    En  = M 
+    Ens = En - ( En - e*sin(En)- M ) / ( 1 - e*cos(En) ) 
+
+    while ( abs(Ens-En) > eps )
+        En  = Ens 
+        Ens = En - (En - e*sin(En) - M) / (1 - e*cos(En)) 
+    end 
+    E = Ens 
+
+    return E 
+end 
 
 
 
