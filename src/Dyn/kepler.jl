82d96735b3982f2c038e91c12dd8dd602a6a6f8e
mis0butter <junetter@gmail.com>
2023-11-20 23:19:20 -0800

    kepler.jl formatting

--- src/Dyn/kepler.jl
+++ src/Dyn/kepler.jl
@@ -1,64 +1,64 @@
 using LinearAlgebra
 
-
-
 ## ============================================ ##
 
-# Function solves Kepler's equation M = E-e*sin(E)
-# Input - Mean anomaly M [rad] , Eccentricity e and Epsilon 
-# Output  eccentric anomaly E [rad]. 
-
-export kepler_H 
+"Function solves Kepler's equation M = E-e*sin(E)" 
 function kepler_H( 
-    M, 
-    e, 
-    eps = 1e-10, 
+    M,              # mean anomaly [rad] 
+    e,              # eccentricity 
+    eps = 1e-10,    # tolerance 
 )
 
-    H_k = M 
+    # starting guess 
+    H_k   = M 
     H_kp1 = H_k + ( M - e*sinh(H_k) + H_k ) / ( e*cosh(H_k) - 1 ) 
 
+    # Newton-Raphson iteration 
     while abs(H_kp1 - H_k) > eps  
         H_k   = H_kp1 
         H_kp1 = H_k + ( M - e*sinh(H_k) + H_k ) / ( e*cosh(H_k) - 1 ) 
     end
     H = H_kp1 
 
-    return H  
+    return H        # hyperbolic anomaly [rad] 
 end 
+
+export kepler_H 
 
 ## ============================================ ##
 
-# Function solves Kepler's equation M = E-e*sin(E)
-# Input - Mean anomaly M [rad] , Eccentricity e and Epsilon 
-# Output  eccentric anomaly E [rad]. 
-
-export kepler_E 
+"Function solves Kepler's equation M = E-e*sin(E)" 
 function kepler_E( 
-    M, 
-    e, 
-    eps = 1e-10, 
+    M,              # mean anomaly [rad] 
+    e,              # eccentricity 
+    eps = 1e-10,    # tolerance 
 )
 
+    # starting guess 
     E_k  = M 
     E_kp1 = E_k - ( E_k - e*sin(E_k)- M ) / ( 1 - e*cos(E_k) ) 
 
-    while ( abs(E_kp1-E_k) > eps )
+    # Newton-Raphson iteration 
+    while abs(E_kp1-E_k) > eps 
         E_k   = E_kp1 
         E_kp1 = E_k - (E_k - e*sin(E_k) - M) / (1 - e*cos(E_k)) 
     end 
     E = E_kp1 
 
-    return E 
+    return E        # eccentric anomaly [rad] 
 end 
 
-## ============================================ ##
+export kepler_E 
 
-export kepler_prop_tof 
+## ============================================ ## 
+
+# Resource: BMW 
+
+"Propagate Keplerian orbit using TOF"
 function kepler_prop_tof( 
-    rv_0, 
-    tof, 
-    mu = 1.0, 
+    rv_0,           # initial position and velocity vectors 
+    tof,            # time of flight 
+    mu = 1.0,       # gravitational parameter 
 )  
 
     # convert to OEs 
@@ -66,14 +66,14 @@ function kepler_prop_tof(
     a    = oe_0[1] 
     e    = oe_0[2] 
     nu_0 = oe_0[6] 
+    
+    # mean motion 
+    n = sqrt( mu / (abs(a))^3 ) 
 
     if e < 1.0 
 
         # initial eccentric anomaly 
         E_0 = acos( ( e + cos(nu_0) ) / ( 1 + e * cos(nu_0) ) ) 
-
-        # mean motion 
-        n = sqrt( mu / a^3 ) 
 
         # mean anomaly - propagate! 
         dM  = n * tof 
@@ -83,21 +83,11 @@ function kepler_prop_tof(
         # find final eccentric anomaly 
         E_f  = kepler_E( M_f, e ) 
         nu_f = acos( ( cos(E_f) - e ) / ( 1 - e*cos(E_f) ) ) 
-        
-        # set target rv 
-        oe_f    = copy(oe_0) 
-        oe_f[6] = nu_f 
-        rv_f    = kep2cart( oe_f, mu ) 
 
     else 
 
-        println( "hyperbolic" ) 
-
         # initial hyperbolic anomaly 
         H_0 = acosh( ( e + cos(nu_0) ) / ( 1 + e * cos(nu_0) ) ) 
-
-        # mean motion 
-        n = sqrt( complex(mu / (-a)^3) ) 
         
         # mean anomaly - propagate! 
         dM  = n * tof 
@@ -108,12 +98,14 @@ function kepler_prop_tof(
         H_f  = kepler_H( M_f, e ) 
         nu_f = 2*atan( sqrt( (e+1)/(e-1) ) * tanh(H_f/2) ) 
 
-        # compute target rv 
-        r_f = a*( 1 - e^2 ) / ( 1 + e * cos(nu_f) ) 
-        v_f = sqrt( mu * ( 2/r_f - 1/a ) ) 
-        rv_f = [r_f ; v_f] 
-
     end 
 
-    return rv_f 
+    # set target rv 
+    oe_f    = copy(oe_0) 
+    oe_f[6] = nu_f 
+    rv_f    = kep2cart( oe_f, mu ) 
+
+    return rv_f     # target rv 
 end 
+
+export kepler_prop_tof 
