ec1c1b207ed1632932c0f75ec74dd390eabed401
mis0butter <junetter@gmail.com>
2023-11-26 02:24:22 -0800

    got augL with eq and ineq constraints to work

--- src/Dyn/test_augL.jl
+++ src/Dyn/test_augL.jl
@@ -4,6 +4,7 @@ using ForwardDiff
 using GLMakie 
 
 ## ============================================ ##
+## ============================================ ##
 # ok ... let's try equality constrained optimization 
 
 # obj fn 
@@ -84,14 +85,9 @@ println( "x min = ", x_k )
 # obj fn 
 obj_fn(x) = (x[1] + 1)^2 + x[2]^2  + x[3]^2 
 
-# want: 
-#   x[1] >= 1 --> -x[1] + 1 <= 0 
-#   x[2] >= 1 --> -x[2] + 1 <= 0 
-# h_fn formulated as <= 0 
-h_fn(x) = [ -x[1] + 1 ; 
-            -x[2] + 1 ] 
-# h_fn(x) = -x[1] + 1  
-ψ_fn    = h_fn 
+# ineq constraints: h_fn formulated as <= 0 
+h_fn(x) = [ -x[1] + 1 ;     # x[1] >= 1 --> -x[1] + 1 <= 0 
+            -x[2] + 1 ]     # x[2] >= 1 --> -x[2] + 1 <= 0 
 N_h     = length( h_fn(x_0) ) 
 
 # lagrange multipliers and penalty parameters 
@@ -106,7 +102,7 @@ tol = 1e-6
 x_0 = [ 2.0, 2.0, 2.0 ] 
 
 # assign  
-fn(x) = augL_fn( x, λ_0, p_0, γ, obj_fn, ψ_fn ) 
+fn(x) = augL_fn( x, λ_0, p_0, γ, obj_fn, h_fn ) 
 
 # gradient fn and compute 
 dfn  = x -> ForwardDiff.gradient( fn, x ) 
@@ -118,16 +114,15 @@ g    = dfn( x_0 )
 λ_k = copy( λ_0 )
 p_k = copy( p_0 ) 
 x_k = copy( x_0 ) 
-ψ_k = ψ_fn( x_k )
+h_k = h_fn( x_k )
 
 k = 0 ; loop = true 
 while loop 
 
     k += 1 
-    println( "k = ", k ) 
 
     # step 1: assign augmented Lagrangian fn 
-    fn(x_k) = augL_fn( x_k, λ_k, p_k, γ, obj_fn, ψ_fn ) 
+    fn(x_k) = augL_fn( x_k, λ_k, p_k, γ, obj_fn, h_fn ) 
     dfn     = x_k -> ForwardDiff.gradient( fn, x_k ) 
 
     # step 2: minimize unconstrained problem  
@@ -140,16 +135,16 @@ while loop
     end 
 
     # update constraint values 
-    ψ_k = ψ_fn( x_min )
+    h_k = h_fn( x_min )
 
     # ... and update parameters 
     for i in eachindex(λ_k)
 
         # update λ
-        λ_k[i] = max( λ_k[i] + p_k[i] * ψ_k[i] , 0.0 ) 
+        λ_k[i] = max( λ_k[i] + p_k[i] * h_k[i] , 0.0 ) 
 
         # update p 
-        if ψ_k[i] > 0 
+        if h_k[i] > 0 
             p_k[i] *= γ 
         end 
 
@@ -161,7 +156,107 @@ while loop
 end 
 
 ## ============================================ ##
+## ============================================ ##
+# let's deal with equality and inequality constraints!!! 
 
+# initial guess 
+x_0 = [ 2.0, 2.0, 2.0 ] 
+
+# obj fn 
+obj_fn(x) = (x[1] + 1)^2 + x[2]^2  + x[3]^2 
+
+# eq constraints 
+c_fn(x) = x[3] - 1 
+N_c     = length( c_fn(x_0) ) 
+
+# ineq constraints: h_fn formulated as <= 0 
+h_fn(x) = [ -x[1] + 1 ;     # x[1] >= 1  
+            -x[2] + 1 ]     # x[2] >= 1  
+N_h     = length( h_fn(x_0) ) 
+
+# put them together 
+ψ_fn(x) = [ c_fn(x) ; h_fn(x) ]  
+N_ψ     = length( ψ_fn(x_0) ) 
+
+# lagrange multipliers and penalty parameters 
+λ_0 = zeros(N_ψ) 
+p_0 = 10.0 * ones(N_ψ) 
+γ   = 2.0 
+
+# define tol 
+tol = 1e-6 
+
+# assign  
+fn(x) = augL_fn( x, λ_0, p_0, γ, obj_fn, ψ_fn ) 
+
+# gradient fn and compute 
+dfn  = x -> ForwardDiff.gradient( fn, x ) 
+g    = dfn( x_0 ) 
+
+## ============================================ ##
+ 
+# step 0: initialize 
+λ_k = copy( λ_0 )
+p_k = copy( p_0 ) 
+x_k = copy( x_0 ) 
+h_k = h_fn( x_k )
+
+k = 0 ; loop = true 
+while loop 
+
+    # step 1: assign augmented Lagrangian fn 
+    fn(x_k) = augL_fn( x_k, λ_k, p_k, γ, obj_fn, ψ_fn ) 
+    dfn     = x_k -> ForwardDiff.gradient( fn, x_k ) 
+
+    # step 2: minimize unconstrained problem  
+    x_min = min_bfgs( fn, dfn, x_k )  
+
+    # step 3 check convergence ... 
+    dx = norm(x_min - x_k) 
+    if ( dx < tol ) && 
+       ( norm(c_fn(x_min)) < tol ) && 
+       ( norm(h_fn(x_min)) < tol ) 
+        loop = false 
+    end 
+
+    # step 4: update multipliers 
+
+    # first split λ and p into eq and ineq constraints 
+    λ_c = λ_k[1:N_c] 
+    p_c = p_k[1:N_c] 
+    λ_h = λ_k[N_c+1:end] 
+    p_h = p_k[N_c+1:end] 
+
+    # deal with equality constraints first 
+    if norm( c_fn(x_min) ) > tol 
+        λ_c += p_c .* c_fn(x_min) 
+        p_c *= γ 
+    end 
+
+    # now inequality constraints 
+    h_k = h_fn( x_min ) 
+    for i in eachindex( λ_h )
+            
+        # update λ
+        λ_h[i] = max( λ_h[i] + p_h[i] * h_k[i] , 0.0 ) 
+
+        # update p 
+        if h_k[i] > 0 
+            p_h[i] *= γ 
+        end 
+
+    end 
+
+    # assign multipliers back in 
+    λ_k = [ λ_c ; λ_h ] 
+    p_k = [ p_c ; p_h ] 
+
+    # update x 
+    x_k = x_min 
+
+end 
+
+println( "x min = ", x_k ) 
 
 
 
