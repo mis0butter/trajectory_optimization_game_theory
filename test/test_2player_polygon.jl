e1242285a17b269bec55c24d35c3e07a597ec951
mis0butter <junetter@gmail.com>
2024-01-26 13:01:46 -0600

    OK I have the state and control hists saved for players 1 and 2. now, currently computing stage cost for each time index for each player

--- test/test_2player_polygon.jl
+++ test/test_2player_polygon.jl
@@ -9,9 +9,9 @@ using Optim
 
 mu = 398600.4415
 r  = 6378.0
-kep0_P = [ r+420.0, 0.1, 20*pi/180, 10.0*pi/180, 20.0*pi/180, 30.0*pi/180 ]
+kep0_P = [ r+420.0, 0.1, 20*pi/180, 10.0*pi/180, 20.0*pi/180, 20.0*pi/180 ]
 rv_0_P = kep2cart(kep0_P, mu) 
-kep0_E = [ r+450.0, 0.2, 10.6*pi/180, 40.0*pi/180, 0.0, 30.0*pi/180 ]
+kep0_E = [ r+450.0, 0.2, 10.6*pi/180, 12.0*pi/180, 0.0, 50.0*pi/180 ]
 rv_0_E = kep2cart(kep0_E, mu) 
 
 # tof for pursuer to catch up to evader 
@@ -42,6 +42,9 @@ fig = plot_polygon( rv_vec, fig )
 # segments 
 N = 10 
 
+# save player state and control hists 
+players = [] 
+
 # ----------------------- #
 # PLAYER ONE 
 
@@ -50,15 +53,25 @@ rv_0 = rv_0_E
 v_f  = rv_E[end,4:6] 
 # Δv_sol = min_Δv( rv_0, rv_f, tof, N, mu ) 
 
+X_vertices = [] 
+U_vertices = [] 
+t_vertices = [] 
 for i in eachindex(vertices)  
 
-    println(i) 
-    rv_f = [ vertices[i] ; v_f ]  
+    rv_f   = [ vertices[i] ; v_f ]  
     Δv_sol = min_Δv_dist( rv_0, rv_f, tof, N, mu ) 
+    t, rv_hist = prop_kepler_tof_Nseg( rv_0, Δv_sol, N, tof / N, mu ) 
+
+    # save hist 
+    push!( X_vertices, rv_hist ) 
+    push!( U_vertices, Δv_sol ) 
+    push!( t_vertices, t ) 
+
     fig    = plot_prop_Δv( rv_0, Δv_sol, N, tof / N, mu, fig )     
 
 end 
 
+push!( players, ( X_vertices = X_vertices, U_vertices = U_vertices, t_vertices = t_vertices ) ) 
 
 # ----------------------- #
 # PLAYER TWO 
@@ -68,14 +81,65 @@ rv_0 = rv_0_P
 v_f  = rv_E[end,4:6] 
 # Δv_sol = min_Δv( rv_0, rv_f, tof, N, mu ) 
 
+X_vertices = [] 
+U_vertices = [] 
+t_vertices = [] 
 for i in eachindex(vertices)  
 
-    println(i) 
-    rv_f = [ vertices[i] ; v_f ]  
+    rv_f   = [ vertices[i] ; v_f ]  
     Δv_sol = min_Δv_dist( rv_0, rv_f, tof, N, mu ) 
+    t, rv_hist = prop_kepler_tof_Nseg( rv_0, Δv_sol, N, tof / N, mu ) 
+
+    # save hist 
+    push!( X_vertices, rv_hist ) 
+    push!( U_vertices, Δv_sol ) 
+    push!( t_vertices, t ) 
+
     fig    = plot_prop_Δv( rv_0, Δv_sol, N, tof / N, mu, fig )     
 
 end 
 
+push!( players, ( X_vertices = X_vertices, U_vertices = U_vertices, t_vertices = t_vertices ) ) 
 
+## ============================================ ## 
+# zero-sum game 
+
+# game cost 
+function stage_cost(x1, x2, u1, u2)
+    sqrt(norm(x1[1:3] - x2[1:3]) + 0.1) + 0.1 * (norm(u1) - norm(u2))
+end
+
+# loop through time corresponding with control inputs 
+us = eachindex( players[begin].U_vertices[begin][:,begin] )
+
+# start with vertex 1 
+i_vert = 1 
+
+cost_matrices = [] 
+
+# loop t hrough time 
+for tt in us 
+
+    x1 = players[1].X_vertices[i_vert][tt,:] 
+    u1 = players[1].U_vertices[i_vert][tt,:] 
+    x2 = players[2].X_vertices[i_vert][tt,:] 
+    u2 = players[2].U_vertices[i_vert][tt,:] 
+
+    # compute cost for player 1 
+    cost = stage_cost( x1, x2, u1, u2 )
+    println( "cost = ", cost )  
+
+    # compute cost for player 2 
+    cost = -stage_cost( x1, x2, u1, u2 )
+    
+end 
+
+
+
+
+
+
+# what do I need? 
+# I need to save the state and control history for each player 
+# I need to save the cost history for each player 
 
