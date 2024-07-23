d12991d884f716be4c13624b859cf4261ebe5ff3
mis0butter <junetter@gmail.com>
2024-07-23 16:39:09 -0500

    working for multiple game steps - and updated plot for markers on desired vertices

--- src/Utils/plotting.jl
+++ src/Utils/plotting.jl
@@ -564,6 +564,12 @@ function plot_p1_p2_traj( game, params, k )
         fig = plot_scatter3d( rv_P[1], rv_P[2], rv_P[3], fig, :circle, :red, 20 ) 
 
     end 
+
+    # plot triangles on chosen vertices 
+    rv_E_des = p1.X[ p1.chosen ][ end, : ] 
+    rv_P_des = p2.X[ p2.chosen ][ end, : ] 
+    fig = plot_scatter3d( rv_E_des[1], rv_E_des[2], rv_E_des[3], fig, :triangle, :blue, 15 ) 
+    fig = plot_scatter3d( rv_P_des[1], rv_P_des[2], rv_P_des[3], fig, :triangle, :red, 15 ) 
     
     # if k > 1 
     if k > 1 
--- test/test_2player_polygon.jl
+++ test/test_2player_polygon.jl
@@ -27,38 +27,9 @@ push!( game.p2_state, players[2] )
 
 game = prop_game_step( game, params, rng ) 
 game = prop_game_step( game, params, rng ) 
-
-# # get most recent player states  
-# rv_E, rv_P = rv_E_P( game, params ) 
-
-# # return reference orbit for most recent step 
-# rv_ref_E, kep_ref_E = find_ref_orbit( game, params ) 
-
-# # generate rv_ref_E_polygon_hist 
-# rv_ref_E_polygon_hist = ref_polygon_hist( kep_ref_E, params ) 
-
-# # now move game forward one step 
-# push!( game.tt, game.tt[end] + params.tt_step ) 
-# push!( game.k_replan, game.k_replan[end] + 1 ) 
-# push!( game.rv_E, rv_E ) 
-# push!( game.rv_P, rv_P ) 
-# push!( game.rv_ref_E_polygon, rv_ref_E_polygon_hist ) 
-
-# # save player state and control hists 
-# p = player_struct( [], [], [], [], [], [], [] ) 
-# players = [ p, deepcopy(p) ]  
-
-# _, rv_E_hist, _, rv_P_hist = prop_rv_E_P( rv_E, rv_P, params ) 
-
-# players[1].rv_0_hist = rv_E_hist 
-# players[2].rv_0_hist = rv_P_hist 
-
-# # compute all possible Δv solutions 
-# players = players_states( params, game, players, rng ) 
-
-# # save player state in game 
-# push!( game.p1_state, players[1] ) 
-# push!( game.p2_state, players[2] ) 
+game = prop_game_step( game, params, rng ) 
+game = prop_game_step( game, params, rng ) 
+game = prop_game_step( game, params, rng ) 
 
 
 ## ============================================ ##
@@ -66,6 +37,6 @@ game = prop_game_step( game, params, rng )
 # plotting stuff 
 
 # k_replan step of the game 
-k = 1 
+k = 5 
 fig = plot_p1_p2_traj( game, params, k )  
 
