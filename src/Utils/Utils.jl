10ff737dcbd2ea7155eeaa42aabe3863237e6cb3
Junette Hsin <junetter@gmail.com>
2024-09-01 18:28:07 -0500

    added rv_ref_hist to p_rv_ref_hist

--- src/Utils/Utils.jl
+++ src/Utils/Utils.jl
@@ -211,13 +211,14 @@ export U_norm
 
 ## ============================================ ##
 
-function p_rv_hist( game, params ) 
+function p_rv_ref_hist( game, params ) 
 
     k_tt_replan = params.k_tt_replan 
     k_max       = game.k_replan[end] 
 
-    p1_rv_hist = [] 
-    p2_rv_hist = [] 
+    p1_rv_hist  = [] 
+    p2_rv_hist  = [] 
+    rv_ref_hist = [] 
     for kk = 1 : k_max 
 
         p1_chosen, p2_chosen = p_strategy( game, kk, params.strategy ) 
@@ -229,25 +230,30 @@ function p_rv_hist( game, params )
             p1_r = game.p1_state[ kk ].X[ p1_chosen ][ 1 : k_tt_replan + 1, : ] 
             p2_r = game.p2_state[ kk ].X[ p2_chosen ][ 1 : k_tt_replan + 1, : ] 
         end 
-    
+
+        rv_ref = game.rv_ref_E[ kk ][ 1 : k_tt_replan + 1, : ] 
+
         push!( p1_rv_hist, p1_r ) 
         push!( p2_rv_hist, p2_r ) 
+        push!( rv_ref_hist, rv_ref ) 
+
     end 
     
-    p1_rv_hist = mapreduce( permutedims, hcat, p1_rv_hist )' 
-    p2_rv_hist = mapreduce( permutedims, hcat, p2_rv_hist )' 
+    p1_rv_hist  = mapreduce( permutedims, hcat, p1_rv_hist )' 
+    p2_rv_hist  = mapreduce( permutedims, hcat, p2_rv_hist )' 
+    rv_ref_hist = mapreduce( permutedims, hcat, rv_ref_hist )' 
 
-    return p1_rv_hist, p2_rv_hist 
+    return p1_rv_hist, p2_rv_hist, rv_ref_hist  
 end 
 
-export p_rv_hist 
+export p_rv_ref_hist 
 
 
 ## ============================================ ## 
 
 function dist_norm( game, params ) 
 
-    p1_rv_hist, p2_rv_hist = p_rv_hist( game, params )
+    p1_rv_hist, p2_rv_hist, rv_ref_hist = p_rv_ref_hist( game, params )
 
     r_diff = p1_rv_hist[:,1:3] - p2_rv_hist[:,1:3] 
     r_norm = [ norm( r_diff[ii,:] ) for ii in 1 : size(r_diff, 1) ] 
