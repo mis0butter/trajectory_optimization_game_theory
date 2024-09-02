83d18ddddee75fd9ee952dbdba3de1df07d8e091
Junette Hsin <junetter@gmail.com>
2024-09-02 03:08:22 -0500

    set up script to run 100 game MC and save outputs

--- test/test_2player_polygon.jl
+++ test/test_2player_polygon.jl
@@ -36,7 +36,7 @@ fig = plot_p1_p2_traj( game, params, N_replan )
 ## ============================================ ##
 # run multiple games 
 
-N_games  = 10 
+N_games  = 100 
 N_replan = 10 
 
 games_vec = [] 
@@ -56,8 +56,72 @@ end
 fig = plot_MC_stats( games_vec, params ) 
 print_MC_stats( games_vec, params ) 
 
+save_games_vec_output( games_vec, params )  
+
 
 ## ============================================ ##
+## ============================================ ##
+# run multiple games 
+
+N_games  = 100 
+N_replan = 10 
+
+games_vec = [] 
+for jj = 1 : N_games 
+
+    params, players, game = init_game( rng, "pure" ) 
+
+    for ii = 1 : N_replan - 1 
+        print( "game: ", jj, " step: ", ii, "\n" ) 
+        game = prop_game_step( game, params, rng ) 
+    end 
+
+    push!( games_vec, game ) 
+
+end 
+
+fig = plot_MC_stats( games_vec, params ) 
+print_MC_stats( games_vec, params ) 
+
+save_games_vec_output( games_vec, params )  
+
+
+## ============================================ ##
+## ============================================ ##
+# run multiple games 
+
+N_games  = 100 
+N_replan = 10 
+
+games_vec = [] 
+for jj = 1 : N_games 
+
+    params, players, game = init_game( rng, "mixed" ) 
+
+    for ii = 1 : N_replan - 1 
+        print( "game: ", jj, " step: ", ii, "\n" ) 
+        game = prop_game_step( game, params, rng ) 
+    end 
+
+    push!( games_vec, game ) 
+
+end 
+
+fig = plot_MC_stats( games_vec, params ) 
+print_MC_stats( games_vec, params ) 
+
+save_games_vec_output( games_vec, params )  
+
+
+
+
+
+
+
+## ============================================ ##
+## ============================================ ##
+## ============================================ ##
+
 
 ii   = 9 
 game = games_vec[ ii ] 
@@ -72,31 +136,47 @@ fig = plot_MC_stats( games_vec, params )
 ## ============================================ ##
 # save as CSV 
 
-# Define the folder and filename
-save_folder   = string("test/results/", params.strategy, "/")
- 
-data0  = [] .* zeros(1,4) 
-header = [ "tt_hist", "p1_rv_hist", "p2_rv_hist", "rv_ref_hist" ] 
-data_frame = DataFrame( data0, header ) 
+function save_games_vec_output( games_vec, params ) 
 
-for ii in eachindex(games_vec) 
+    # Define the folder and filename
+    save_folder   = string("test/results/", params.strategy, "/")
+    
+    data0  = [] .* zeros(1,4) 
+    header = [ "tt_hist", "p1_rv_hist", "p2_rv_hist", "rv_ref_hist" ] 
+    data_frame = DataFrame( data0, header ) 
 
-    game_folder = string(save_folder, "game_", ii,"/") 
+    for ii in eachindex(games_vec) 
 
-    if !isdir(game_folder)
-        mkdir(game_folder)
+        game_folder = string(save_folder, "game_", ii,"/") 
+
+        if !isdir(game_folder)
+            mkdir(game_folder)
+        end 
+
+        game = games_vec[ ii ] 
+        
+        tt_hist, p1_rv_hist, p2_rv_hist, rv_ref_hist = p_rv_ref_hist( game, params ) 
+
+    
+        CSV.write( string(game_folder, "tt_hist.csv"), DataFrame(tt_hist, :auto) ) 
+        CSV.write( string(game_folder, "p1_rv_hist.csv"), DataFrame(p1_rv_hist, :auto) )  
+        CSV.write( string(game_folder, "p2_rv_hist.csv"), DataFrame(p2_rv_hist, :auto) ) 
+        CSV.write( string(game_folder, "rv_ref_hist.csv"), DataFrame(rv_ref_hist, :auto) ) 
+        
     end 
 
-    game = games_vec[ ii ] 
-    
-    tt_hist, p1_rv_hist, p2_rv_hist, rv_ref_hist = p_rv_ref_hist( game, params ) 
-
-   
-    CSV.write( string(game_folder, "tt_hist.csv"), DataFrame(tt_hist, :auto) ) 
-    CSV.write( string(game_folder, "p1_rv_hist.csv"), DataFrame(p1_rv_hist, :auto) )  
-    CSV.write( string(game_folder, "p2_rv_hist.csv"), DataFrame(p2_rv_hist, :auto) ) 
-    CSV.write( string(game_folder, "rv_ref_hist.csv"), DataFrame(rv_ref_hist, :auto) ) 
-    
 end 
+
+
+## ============================================ ##
+# read CSV 
+
+ii = 1 
+game_folder = string(save_folder, "game_", ii,"/") 
+filename    = string(game_folder, "tt_hist.csv")
+
+tt_hist     = CSV.read( filename, DataFrame ) 
+
+
 
 
