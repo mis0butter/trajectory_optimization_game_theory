5115d52888b6d826e86b930d0d690736da93599a
Junette Hsin <junetter@gmail.com>
2024-09-02 18:13:49 -0500

    updated saving outputs fn to use jld for saving variables

--- test/test_2player_polygon.jl
+++ test/test_2player_polygon.jl
@@ -59,62 +59,17 @@ print_MC_stats( games_vec, params )
 save_games_vec_output( games_vec, params )  
 
 
-## ============================================ ##
-## ============================================ ##
-# run multiple games 
 
-N_games  = 100 
-N_replan = 10 
-
-games_vec = [] 
-for jj = 1 : N_games 
-
-    params, players, game = init_game( rng, "pure" ) 
-
-    for ii = 1 : N_replan - 1 
-        print( "game: ", jj, " step: ", ii, "\n" ) 
-        game = prop_game_step( game, params, rng ) 
-    end 
-
-    push!( games_vec, game ) 
-
-end 
-
-fig = plot_MC_stats( games_vec, params ) 
-print_MC_stats( games_vec, params ) 
-
-save_games_vec_output( games_vec, params )  
 
 
 ## ============================================ ##
-## ============================================ ##
-# run multiple games 
+# save 
 
-N_games  = 100 
-N_replan = 10 
+save_folder   = string( "test/results/", params.strategy, "/" ) 
+filename      = string( "games_", N_games, ".jld2" ) 
+full_filename = string( save_folder, filename ) 
 
-games_vec = [] 
-for jj = 1 : N_games 
-
-    params, players, game = init_game( rng, "mixed" ) 
-
-    for ii = 1 : N_replan - 1 
-        print( "game: ", jj, " step: ", ii, "\n" ) 
-        game = prop_game_step( game, params, rng ) 
-    end 
-
-    push!( games_vec, game ) 
-
-end 
-
-fig = plot_MC_stats( games_vec, params ) 
-print_MC_stats( games_vec, params ) 
-
-save_games_vec_output( games_vec, params )  
-
-
-
-
+@save full_filename games_vec params 
 
 
 
@@ -138,32 +93,15 @@ fig = plot_MC_stats( games_vec, params )
 
 function save_games_vec_output( games_vec, params ) 
 
-    # Define the folder and filename
-    save_folder   = string("test/results/", params.strategy, "/")
-    
-    data0  = [] .* zeros(1,4) 
-    header = [ "tt_hist", "p1_rv_hist", "p2_rv_hist", "rv_ref_hist" ] 
-    data_frame = DataFrame( data0, header ) 
-
-    for ii in eachindex(games_vec) 
-
-        game_folder = string(save_folder, "game_", ii,"/") 
-
-        if !isdir(game_folder)
-            mkdir(game_folder)
-        end 
-
-        game = games_vec[ ii ] 
-        
-        tt_hist, p1_rv_hist, p2_rv_hist, rv_ref_hist = p_rv_ref_hist( game, params ) 
-
-    
-        CSV.write( string(game_folder, "tt_hist.csv"), DataFrame(tt_hist, :auto) ) 
-        CSV.write( string(game_folder, "p1_rv_hist.csv"), DataFrame(p1_rv_hist, :auto) )  
-        CSV.write( string(game_folder, "p2_rv_hist.csv"), DataFrame(p2_rv_hist, :auto) ) 
-        CSV.write( string(game_folder, "rv_ref_hist.csv"), DataFrame(rv_ref_hist, :auto) ) 
-        
+    save_folder   = string( "test/results/", params.strategy, "/" ) 
+    if !isdir(save_folder)
+        mkdir(save_folder)
     end 
+
+    filename      = string( "games_", N_games, ".jld2" ) 
+    full_filename = string( save_folder, filename ) 
+
+    @save full_filename games_vec params 
 
 end 
 
